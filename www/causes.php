44bc8062b8e0908b4cd72d128a783c91bf70206b
Sebastián Rojas <sebastian@iridian.co>
2016-06-20 03:34:55 -0500

    VARIOUS

--- www/causes.php
+++ www/causes.php
@@ -16,7 +16,7 @@ $cause = $causes[array_search($_GET['id'], array_column($causes, 'id'))];
         <meta property="og:title" content="<?php echo $cause['name'] ?> - El Activista | El Chapín Prensa" />
         <meta property="og:description" content="<?php echo $cause['description'] ?>" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="http://elactivista.elchapin.co/causes/<?php echo $cause['id'] ?>" />
+        <meta property="og:url" content="http://elactivista.elchapin.co/causes/<?php echo $cause['id'] ?>/" />
         <meta property="og:image" content="http://elactivista.elchapin.co/causes/<?php echo $cause['id'] ?>/img/og.png" />
         <meta property="og:image:width" content="600" />
         <meta property="og:image:height" content="323" />
